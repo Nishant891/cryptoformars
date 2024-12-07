90627a00b037d2dd3925f46b1145d1054ca9b37b
Nishant Sharma <nishant19072003@gmail.com>
2024-12-07 19:52:44 +0530

    bug fixes 4

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 import { getBlogMetadata } from "../functions";
 import BlogList from "../components/BlogList";
 
-export default function BlogIndex({ params }: { params: { slug: string } }) {
+export default function BlogIndex() {
   const blogs = getBlogMetadata();
   return (
     <div style={{ display: "flex" }}>
