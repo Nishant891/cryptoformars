5259d87ee7c98c63e3892afb16089e43bf7c590a
Nishant Sharma <nishant19072003@gmail.com>
2024-12-18 20:50:11 +0530

    bug fixes 8

--- app/components/sub-components/AppLogo.tsx
+++ app/components/sub-components/AppLogo.tsx
@@ -1,18 +1,17 @@
 import type { DetailedHTMLProps, HTMLAttributes } from "react";
-import Logo from "@/public/assets/green_alien_head_vector_logo-removebg-preview.png"
 import Image from "next/image";
 
 type Props = DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>;
 
 export default function AppLogo({ className, ...props }: Props) {
     return (
-        <h2 {...props} className={`text-4xl font-redzone app-logo ${className}`}>
-            <Image alt="Logo" width={60} height={60} src={Logo}></Image>
+        <h2 {...props} className={`text-2xl font-redzone app-logo ${className}`}>
+            <span>CryptoForMars</span>
         </h2>
     );
     return (
-        <h2 className="font-redzone text-4xl bg-gradient-to-br from-app via-fuchsia-300/80 bg-clip-text text-transparent">
-            <Image alt="Logo" src={Logo}></Image>
+        <h2 className="font-redzone text-2xl bg-gradient-to-br from-app via-fuchsia-300/80 bg-clip-text text-transparent">
+            <span>CryptoForMars</span>
         </h2>
     );
 }
